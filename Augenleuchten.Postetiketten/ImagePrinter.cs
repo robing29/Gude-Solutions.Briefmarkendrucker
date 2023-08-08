60422ad70038d724024d45d99499e259eaaa9aee
Robin Gude <guderobin@gmail.com>
2023-08-09 00:09:45 +0200

    - Deleted deprecated comments

--- Augenleuchten.Postetiketten/ImagePrinter.cs
+++ Augenleuchten.Postetiketten/ImagePrinter.cs
@@ -28,24 +28,14 @@ namespace Augenleuchten.Postetiketten
             // Set printing margins to zero
             pd.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
 
-            pd.PrinterSettings.PrinterName =  FormMainWindow.PrinterName == null ? "MUNBYN itpp 130b" : FormMainWindow.PrinterName;
+            pd.PrinterSettings.PrinterName = FormMainWindow.PrinterName == null ? "MUNBYN itpp 130b" : FormMainWindow.PrinterName;
             // Print the document
             pd.Print();
         }
 
         private void PrintPageHandler(object sender, PrintPageEventArgs e)
         {
-            // Calculate scaling factors
-            //float scaleX = (float)e.MarginBounds.Width / imageToPrint.Width;
-            //float scaleY = (float)e.MarginBounds.Height / imageToPrint.Height;
-            //float scale = Math.Min(scaleX, scaleY);
 
-            //// Calculate the position to center the image
-            //float xPos = e.MarginBounds.Left + (e.MarginBounds.Width - imageToPrint.Width * scale) / 2;
-            //float yPos = e.MarginBounds.Top + (e.MarginBounds.Height - imageToPrint.Height * scale) / 2;
-
-            //// Draw the scaled image
-            //e.Graphics.DrawImage(imageToPrint, xPos, yPos, imageToPrint.Width * scale, imageToPrint.Height * scale);
             e.Graphics.DrawImage(imageToPrint, e.MarginBounds);
 
             e.HasMorePages = false; // No more pages to print
